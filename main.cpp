41c8c07f0458107f427289d631e5a2154b4d9b0f
Max Botviniev <max-bot@outlook.com>
2015-10-28 19:07:27 +0200

    Blank Board output

--- main.cpp
+++ main.cpp
@@ -12,16 +12,18 @@
 
 using std::cout;
 
-//FRONTEND
+//FRONTEND Classes
 //-----------------------
 
 class Tile {
-    const static unsigned int lines_per_tile_c = 3;
-    char * lines_v[lines_per_tile_c];
 public:
+    const static unsigned int CHARS_PER_ROW = 6;
+    const static unsigned int LINES = 3;
     Tile( bool color_v, char info_v[] );
     char * GetLine( unsigned int index );
     ~Tile();
+private:
+    char * lines_v[LINES];
 };
 
     Tile::Tile( bool color_v, char info_v[] ) {
@@ -50,47 +52,75 @@ char * Tile::GetLine( unsigned int index ) {
 }
 
 Tile::~Tile() {
-    for( int i = 0; i < lines_per_tile_c; i++)
+    for( int i = 0; i < LINES; i++)
     {
         delete lines_v[i];
     }
 }
 
-//----------------------
+//FRONTEND
+//-----------------------
 
-void Frontend( /*char * board_state_r*/ ) {
-
-    char tile_info[] =  "BPB2";
-    Tile tileWhite( true, tile_info );
-    Tile tileBlack( false, tile_info );
-    
-    bool whiteFirst = false;
-    int index = 0;
-    for( int y = 0; y < 8 * 3; y++ ) {
-        
-        index = index < 2 ? ( (y+3) % 3) : y % 3;
-        if( index % 3 == 0 ) whiteFirst = !whiteFirst;
-        
-        for( int x = 0; x < 8; x++ ) {
-            
-            if(x % 2 == 0 && whiteFirst) {
-                cout << tileWhite.GetLine( index );
-            }
-            else {
-                cout << tileBlack.GetLine( index );
-            }
-        }
-        
-        cout << std::endl;
-    }
+inline unsigned int ClampTilesRowOrder( unsigned int index )
+{
+    return index < ( Tile::LINES -1 ) ? ( index +1 ) : 0;
 }
+
+//-----------
+
+char * GetOutput( /*char * board_state_r*/ ) {
+
+    //Data
+    //-----------
+    unsigned int chars_per_row_l = Tile::CHARS_PER_ROW * 8; // 6 horisontal chars for Tile * 8 horizontal Tiles per Board
+    unsigned int rows_per_board_l = Tile::LINES * 8; // 3 rows per Tile * 8 vertical Tiles per Board
+    
+    unsigned int output_length_l =  chars_per_row_l * rows_per_board_l +
+                                    //rows_per_board_l + // '\n' for each row
+                                    1; // for '\0' terminator
+    
+    char tile_info[] =  "BPB2";
+    Tile whiteTile( true, tile_info );
+    Tile blackTile( false, tile_info );
+    
+    
+    //Fill
+    //-----------
+    char * resullt_p = new char[ output_length_l ];
+    
+    unsigned int vertical_tile_char_index_l = -1;
+    
+    for( int y = 0; y < output_length_l -1; y++ ) {
+        
+        //If board line ends
+        if( y % chars_per_row_l == 0 ) {
+            //Start new line
+            resullt_p[ y ] = '\n';
+            //Increment line content index
+            vertical_tile_char_index_l = ClampTilesRowOrder( vertical_tile_char_index_l );
+        }
+        else {
+            //Increment symbol of line
+            unsigned int horizontal_tile_char_index_l = y % Tile::CHARS_PER_ROW;
+            //Insert current symbol to output text
+            resullt_p[ y ] = blackTile.GetLine( vertical_tile_char_index_l )[ horizontal_tile_char_index_l ];
+        }
+    }
+    resullt_p[ output_length_l -1 ] = '\0';
+    
+    return resullt_p;
+}
+
+//-----------------------
 
 int main(int argc, const char * argv[]) {
     
     //BCAI::Communicator communicator;
     //string output = communicator.GetDecision("KA1");
-   
-    Frontend();
+    
+    char * output = GetOutput();
+    cout << output;
+    delete[] output;
     
     return 0;
 }
